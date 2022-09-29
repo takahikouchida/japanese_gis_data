b17eb07d3f392a6453d484d095a35747cbd36f13
takahikouchida <EH2UbZiHcQ3d>
2022-09-29 16:13:56 +0900

    国土数値情報 人口集中地区データ

---
+++ DID/平成27年/toSQL.sh
@@ -0,0 +1,8 @@
+unzip "*.zip"
+
+files=($(find . -regex ".*.shp"))
+shp2pgsql -p -W cp932 -i -I -D -s 4326 ${files[0]} did2015 > did2015.sql
+
+for filepath in "${files[@]}";do
+ shp2pgsql -a -W cp932 -i -D -s 4326 $filepath did2015 >> did2015.sql
+ done
